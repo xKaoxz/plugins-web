7479eb745ec751c6f8ea77984e5d9a9cc7bd3689
xKaoxz <xkaoxzz@gmail.com>
2026-03-05 10:47:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const search = document.getElementById("search");
-const plugins = document.querySelectorAll(".plugin");
+const search = document.getElementById("search")
+
+const plugins = document.querySelectorAll(".plugin")
 
 search.addEventListener("keyup", () => {
 
-let text = search.value.toLowerCase();
+let text = search.value.toLowerCase()
 
 plugins.forEach(plugin => {
 
-let name = plugin.innerText.toLowerCase();
+let name = plugin.innerText.toLowerCase()
 
-plugin.style.display = name.includes(text)
+plugin.style.display =
+name.includes(text)
 ? "block"
-: "none";
+: "none"
 
-});
+})
 
-});
+})
